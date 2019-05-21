726ee6cde44799b4c19cca8f6ba5edbbadde7a11
Miguel Angel Quezada Galvan <miguel.quezadag@alumno.udg.mx>
2019-05-21 17:39:00 -0500

    Se finalizo API Material

--- application/controllers/Material.php
+++ application/controllers/Material.php
@@ -18,7 +18,7 @@ class Material extends REST_Controller {
 			$folio = $this->post('folio');
 			$reporteProyecto = $this->post('reporteProyecto');
 			$solicita = $this->post('solicita');
-			$material = $this->post('matrial');
+			$material = $this->post('material');
 			$catalogo = $this->post('catalogo');
 			$unidad = $this->post('unidad');
 			$cantidadSolicitada = $this->post('cantidadSolicitada');
@@ -69,7 +69,7 @@ class Material extends REST_Controller {
 		$this->db->insert('materialList',$datos);
 		//SE ENVIA LA RESPUESTA
 		$respuesta = array('error' => FALSE,
-							'mensaje' => 'Se ha realizado el reporte correctamente');
+							'mensaje' => 'Se ha realizado el reporte correctamente, puede cerrar la ventana ahora');
 
 		$this->response($respuesta);  
 	}
@@ -122,6 +122,34 @@ class Material extends REST_Controller {
 			$diasSurtido = $this->post('diasSurtido');
 			$estatus = $this->post('estatus');
 			$observaciones = $this->post('observaciones');
+
+			if ($diasSurtido == 'N/A') {
+				$diasSurtido = null;
+			}
+			if($material == 'N/A'){
+				$material = null;
+			}
+			if($catalogo == 'N/A'){
+				$material = null;
+			}
+			if($unidad == 'N/A'){
+				$unidad = null;
+			}
+			if($cantidadSolicitada == 'N/A'){
+				$cantidadSolicitada = null;
+			}
+			if($cantidadRecibida == 'N/A'){
+				$cantidadRecibida = null;
+			}
+			if($estatus == 'N/A'){
+				$estatus = null;
+			}
+			if($observaciones == 'N/A'){
+				$observaciones = null;
+			}
+			if($fecha == 'N/A'){
+				$fecha = null;
+			}
 
 			$condiciones = array('reporte_proyecto' => $reporteProyecto,
 										'solicita' => $solicita,
