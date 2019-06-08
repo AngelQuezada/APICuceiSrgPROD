2f71f29acb3afb5fcc56d6211051977d0c79a412
Miguel Angel Quezada Galvan <miguel.quezadag@alumno.udg.mx>
2019-06-08 11:18:20 -0500

    Creacion API Infraestructura

--- application/controllers/Infraestructura.php
+++ application/controllers/Infraestructura.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require_once(APPPATH.'/libraries/REST_Controller.php');
 use Restserver\Libraries\REST_Controller;
 
-class Aula extends REST_Controller {
+class Infraestructura extends REST_Controller {
+
 	public function __construct()
 	{
 		header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
@@ -17,7 +18,15 @@ class Aula extends REST_Controller {
 	public function index(){
 	}
 
-	public function aulas_get($module_id,$floor_id){
+	public function imodulos_get(){
+		$query = $this->db->query("SELECT id,module_name FROM moduleList");
+			$this->response($query->result());
+    }
+    public function ipisos_get($module_id){
+		$query = $this->db->query("SELECT DISTINCT floor_id FROM aulaList WHERE module_id='".$module_id."'");
+			$this->response($query->result());
+    }
+    public function iaulas_get($module_id,$floor_id){
 		$query = $this->db->query("SELECT aula_name FROM aulaList WHERE module_id='".$module_id."' AND floor_id='".$floor_id."'");
 			$this->response($query->result());
 	}
